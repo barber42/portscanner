1160a119da1094d106f1eeb1350923069b2cd9ea
Brighton Barber <bkbarber42@gmail.com>
2020-10-16 15:54:35 -0600

    Not fully tested but it should be able to handle all 3 protocols

--- script/prtscn.py
+++ script/prtscn.py
@@ -8,13 +8,19 @@ from datetime import datetime
 #Clear the screen
 subprocess.call('clear', shell=True)
 
-#Ask for input
+#Assign protocols
+proto = socket.getprotobyname('tcp')
+imcp = socket.getprotobyname('icmp')
+udp = socket.getprotobyname('udp')
+
+#Ask for host
 remoteServer = raw_input("Enter a remote host to scan: ")
 if remoteServer.isalpha():
     remoteServerIP = socket.gethostbyname(remoteServer)
 else:
     remoteServerIP= socket.gethostbyaddr(remoteServer)[2][0]
 
+#Ask for port
 portInput = raw_input("Enter the port (##) or range of ports (##-##) to be scanned: ")
 if "-" in portInput:
     port1 = int(portInput.split("-")[0])
@@ -24,12 +30,23 @@ else:
     port2 = int(portInput) + 1
     # port2 = portInput
 
-# print remoteServerIP
+#Ask which protocol should be used
+protoName = "TCP"
+
+protoGet = raw_input("Which protocol? (1)TCP (2)UDP (3)ICMP: ")
+if protoGet == '2':
+    print "here"
+    proto = udp
+    protoName = "UDP"
+elif protoGet == '3':
+    proto = imcp
+    protoName = "IMCP"
+
 
 #Print a nice banner with information on which host we are about to scan
-print "-" * 60
-print "Please wait, scanning remote host", remoteServerIP
-print "-" * 60
+print "-" * 80
+print "Please wait, scanning remote host", remoteServerIP , " with ", protoName, " protocol"
+print "-" * 80
 
 #Check what time the scan started
 t1 = datetime.now()
@@ -41,7 +58,7 @@ t1 = datetime.now()
 try:
     for port in range (port1,port2):
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto)
 
         result = sock.connect_ex((remoteServerIP, port))
 
